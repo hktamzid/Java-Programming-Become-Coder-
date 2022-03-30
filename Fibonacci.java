037cf461e8c1b3182b4d083e49f7e72100b98381
hktamzid <20mh1a0536@acoe.edu.in>
2022-03-30 11:28:12 +0530

    New code

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) throws Exception
     {
-        int a= 0, b = 1, c,n,i;
+        int a= 0, b = 1, c,n,i, sum = 0;
         Scanner S = new Scanner(System.in);
         System.out.print("Enter a number: ");
         n = S.nextInt();
@@ -11,11 +11,14 @@ public class Fibonacci {
         c = a + b;
         while(c <= n)
         {
+            if(c%2==0)
+                sum = sum + c;
             a = b;
             b = c;
             System.out.print(c+" ");
             c = a + b;
         }
+        System.out.println("\n"+sum);
 
         /*for(i = 3;i <= n; i++)
         {
