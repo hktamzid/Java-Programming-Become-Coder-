050fc27c7e5e5f7e41d892dad17df17730bfc27b
hktamzid <20mh1a0536@acoe.edu.in>
2022-03-30 10:54:06 +0530

    New code

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,17 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class SwitchCase {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
         char ch;
-        Scanner s = new Scanner(System.in);
-        char = s.nextLine();
+        System.out.print("Enter a Capital Letter ");
+        ch =(char)br.read();
         switch(ch)
         {
-            case 'A' || 'a':
-            case 'E' || 'e':
-            case 'I' || 'i':
-            case 'O' || 'u':
-            case 'u' || 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
                 System.out.println("Vowel");
                 break;
             default:
