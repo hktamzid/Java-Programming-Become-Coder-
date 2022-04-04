06ce5d9edf116cc7157bcf4e209f48d0e12b32e9
hktamzid <20mh1a0536@acoe.edu.in>
2022-04-04 11:09:18 +0530

    String4

---
+++ String4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class String4 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1, str2 = "";
+        int digit = 0, sum = 0;
+        str1 = br.readLine();
+
+        char ch[] = str1.toCharArray();
+        for(int i = 0; i < ch.length; i++)
+        {
+            //if(Character.isLetter(ch[i]));
+                           //str2 = str2 + ch[i];
+            if(Character.isDigit(ch[i]))
+            //digit++;
+            sum = sum + Integer.parseInt(ch[i]+"");
+        }
+        System.out.println(str2);
+        System.out.println(sum);
+    }
+}
