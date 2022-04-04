a2b0873e995a61c33668ddc101e49db25e93fc9a
hktamzid <20mh1a0536@acoe.edu.in>
2022-04-04 12:17:39 +0530

    String test1

---
+++ String7.java
@@ -0,0 +1,11 @@
+public class String7 {
+    public static void main(String[] args) {
+        String ta = "A";
+        ta = ta.concat("B");
+        String tb = "C";
+        ta = ta.concat(tb);
+        ta.replace('C','D');
+        ta = ta.concat(tb);
+        System.out.println(ta);
+    }
+}
