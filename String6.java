773e2bf92dfe7d18e124c84a1b37024be516f1cd
hktamzid <20mh1a0536@acoe.edu.in>
2022-04-04 11:45:40 +0530

    String 6

---
+++ String6.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class String6 {
+    public static void main(String[] args) throws Exception
+    {
+        StringBuffer s1 = new StringBuffer();
+        System.out.println(s1.capacity());
+        System.out.println(s1.length());
+
+        s1.append("Ami banglay gan gai");
+        System.out.println(s1.capacity());
+        System.out.println(s1.length());
+        StringBuffer s2 = new StringBuffer("Welcome");
+
+        System.out.println(s2.charAt(4));   //o
+        s2.setCharAt(4,'o');
+        System.out.println(s2);             //Welcome
+
+        s2.deleteCharAt(4);
+        System.out.println(s2);             //Welcme
+
+        s2.append(" Tamzid");
+        System.out.println(s2);            // Welcme Tamzid
+
+        s2.insert(4,'o');
+        System.out.println(s2);            //Welcome Tamzid
+
+        s2.delete(8,14);
+        System.out.println(s2);            //Welcome
+    }
+}
